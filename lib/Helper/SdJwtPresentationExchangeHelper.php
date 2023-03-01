cd27a14e4b044448d4ca374090526b432665e9e5
Fabian Hauck <fabianh@yes.com>
2023-03-01 11:02:37 +0100

    Fix input_descriptors array

--- lib/Helper/SdJwtPresentationExchangeHelper.php
+++ lib/Helper/SdJwtPresentationExchangeHelper.php
@@ -12,28 +12,29 @@ class SdJwtPresentationExchangeHelper {
         return array(
             "id" => $presentationID,
             "input_descriptors" => array(
-                "id" => SdJwtPresentationExchangeHelper::INPUT_DESCRIPTOR_ID,
-                "format" => array(
-                    "vc+sd-jwt" => array(
-                        "proof_type" => array("JsonWebSignature2020")
-                    )
-                ),
-                "constraints" => array(
-                    "limit_disclosure" => "required",
-                    "fields" => array(
-                        array(
-                            "path" => array("$.type"),
-                            "filter" => array(
-                                "type" => "string",
-                                "const" => "VerifiedEMail"
+                array(
+                    "id" => SdJwtPresentationExchangeHelper::INPUT_DESCRIPTOR_ID,
+                    "format" => array(
+                        "vc+sd-jwt" => array(
+                            "proof_type" => array("JsonWebSignature2020")
+                        )
+                    ),
+                    "constraints" => array(
+                        "limit_disclosure" => "required",
+                        "fields" => array(
+                            array(
+                                "path" => array("$.type"),
+                                "filter" => array(
+                                    "type" => "string",
+                                    "const" => "VerifiedEMail"
+                                )
+                            ),
+                            array(
+                                "path" => array("$.credentialSubject.email")
                             )
-                        ),
-                        array(
-                            "path" => array("$.credentialSubject.email")
                         )
                     )
                 )
-
             )
         );
     }
